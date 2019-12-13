5e805e8faaf828b1707e22cb82f33a9d7d11cdfb
Taito Kutsuzawa <taklimone12uvgtml@gmail.com>
2019-12-13 17:37:57 +0900

    put kruskal in namespace tklib

--- include/graph/kruskal.hpp
+++ include/graph/kruskal.hpp
@@ -1,28 +1,33 @@
-#include "data_structure/union_find.hpp"
+#ifndef TKLIB_KRUSKAL_HPP
+#define TKLIB_KRUSKAL_HPP
 
+#include "data_structure/union_find.hpp"
 #include <algorithm>
 #include <vector>
-using namespace std;
+#include <tuple>
+
+namespace tklib {
 
 template <typename T>
-struct edge{
-    int from;
-    int to;
-    T cost;
-};
+auto kruskal(std::size_t V, std::vector<std::tuple<int, int, T>> &G) {
+    using edge_type = std::tuple<int, int, T>;
 
-template <typename T>
-vector<edge<T>> kruskal(size_t V, vector<edge<T>> &G) {
-    sort(begin(G), end(G), 
-    [](const edge<T> &lhs, const edge<T> &rhs){ return lhs.cost < rhs.cost; });
-    union_find uf(V);
+    std::sort(begin(G), end(G), 
+    [](const edge_type &lhs, const edge_type &rhs)
+    { return std::get<2>(lhs) < std::get<2>(rhs); });
+    
+    tklib::UnionFind uf(V);
+    std::vector<edge_type> edgelist;
 
-    vector<edge<T>> edgelist;
-    for(auto e : G) {
-        if(uf.unite(e.from, e.to)) {
-            edgelist.push_back(e);
+    for(edge_type &edge : G) {
+        if(uf.unite(std::get<0>(edge), std::get<1>(edge))) {
+            edgelist.push_back(edge);
         }
     }
 
     return edgelist;
 }
+
+} // namespace tklib
+
+#endif // TKLIB_KRUSKAL_HPP
--- test/graph/kruskal.cpp
+++ test/graph/kruskal.cpp
@@ -1,23 +1,23 @@
 #define PROBLEM http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_2_A
 
 #include "graph/kruskal.hpp"
-
 #include <iostream>
 #include <vector>
+#include <tuple>
 using namespace std;
 
 int main() {
     int V, E;
     cin >> V >> E;
-    vector<edge<int>> G;
+    vector<tuple<int, int, int>> G;
     while(E--) {
         int s, t, w;
         cin >> s >> t >> w;
-        G.push_back({s, t, w});
+        G.emplace_back(s, t, w);
     }
 
-    auto edgelist = kruskal<int>(V, G);
+    auto edgelist = tklib::kruskal(V, G);
     int weight_sum = 0;
-    for(auto e : edgelist) weight_sum += e.cost;
-    cout << weight_sum << endl;
+    for(auto e : edgelist) weight_sum += get<2>(e);
+    cout << weight_sum << '\n';
 }
